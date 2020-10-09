34a98c5144e54a543a9ae2f0c6b06361581df585
Anjali Dixit <anjalidixitlive@gmail.com>
2020-10-09 23:39:07 +0530

    cm

--- src/com/company/Factorial.java
+++ src/com/company/Factorial.java
@@ -12,6 +12,6 @@ public class Factorial {
         {
             factorial = factorial * i;
         }
-        System.out.println(" Factorial of " +number+" is : " +factorial);
+        System.out.println(" Factorial of n " +number+" is : " +factorial);
     }
 }
